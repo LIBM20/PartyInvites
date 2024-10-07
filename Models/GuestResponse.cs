a9a5ac09d0b3c9ce4d5cdf2d0fa3c308f96bd72a
LIBM20 <libm20@outlook.com>
2024-10-07 15:38:05 +0100

    Modelo para respostas para a festa :)

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        /* Em Java
+        public string name;
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }*/
+
+        public string Name { get; set; }
+        public string Phone {  get; set; }
+        public string Email { get; set; }
+        //pode ser usado um ponto de interrogação a seguir ao bool para caso possa ser Null
+        public bool WillAttend { get; set; }
+
+
+        
+    }
+}
